a94be0bbbc210278454e0498a09e337fbbc2b569
unknown <philipho827@gmail.com>
2020-06-27 17:15:22 +0800

    updated config

--- flask_web_ui/config.py
+++ flask_web_ui/config.py
@@ -2,9 +2,9 @@ import os           # If you need to use OS environment variables
 
 class Config:
     SECRET_KEY = 'b319157f1a11c07c0160d3f940ba2a87'
-    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+    #SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-    #SQLALCHEMY_DATABASE_URI = 'mysql://philipsqladmin:Integra827@philipmysql.mysql.database.azure.com:3306/flaskdb'
+    SQLALCHEMY_DATABASE_URI = 'mysql://philipsqladmin:Integra827@philipmysql.mysql.database.azure.com:3306/flaskdb'
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
